00aa5c08966b079e5b5d63cb1c8b5e6002289751
Pavlo Ohol <3070799pp@gmail.com>
2022-07-21 10:06:08 +0300

    Character limit exceeded bug fixed

--- src/TranslationService.py
+++ src/TranslationService.py
@@ -10,13 +10,27 @@ class TranslationService:
 
     def translate(self, source_text: str):
         try:
-            response = requests.post(url=TRANSLATION_URL,
-                                     data={
-                                         'q': source_text,
-                                         'source': "de",
-                                         'target': "ru",
-                                         'format': "html",
-                                     })
-            return response.json()['translatedText']
+            if len(source_text) < 3000:
+                response = requests.post(url=TRANSLATION_URL,
+                                         data={
+                                             'q': source_text,
+                                             'source': "de",
+                                             'target': "ru",
+                                             'format': "html",
+                                         })
+                return response.json()['translatedText']
+            else:
+                split_str = [source_text[0:len(source_text)//2], source_text[len(source_text)//2:]]
+                translated_split_str = []
+                for s in split_str:
+                    response = requests.post(url=TRANSLATION_URL,
+                                             data={
+                                                 'q': s,
+                                                 'source': "de",
+                                                 'target': "ru",
+                                                 'format': "html",
+                                             })
+                    translated_split_str.append(response.json()['translatedText'])
+                    return translated_split_str[0][:-4] + translated_split_str[1] + "</p>"
         except Exception:
             return None
